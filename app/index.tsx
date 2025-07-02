cc6c020ed41f9f1db1b8b7f8f61dcb61e706c8ab
Junichi Kako <cjaucono@gmail.com>
2025-07-02 14:41:16 +0900

    起動画面の作成

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function InitialScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>アプリ起動中・・・</Text>
     </View>
   );
 }
@@ -13,8 +11,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
