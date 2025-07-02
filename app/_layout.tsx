a9825eb1af601189b0c8c576df0c23ce963e4ea3
Junichi Kako <cjaucono@gmail.com>
2025-07-02 15:02:21 +0900

    feat: 画面推移の設定

---
+++ app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerTintColor: '#000',
+        headerStyle: { backgroundColor: '#f9f9f9' },
+      }}
+    >
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+      {/* ホーム */}
+      <Stack.Screen name='home/index' options={{ headerTitle: 'ホーム' }} />
+
+      {/* ラベル */}
+      <Stack.Screen
+        name='labels'
+        options={{ headerShown: false, presentation: 'fullScreenModal' }}
+      />
+      {/* メモ */}
+      <Stack.Screen name='memos/index' options={{ headerTitle: 'メモ' }} />
+      <Stack.Screen name='memo/create' options={{ headerTitle: '' }} />
+      <Stack.Screen name='memo/[id]' options={{ headerTitle: '' }} />
+    </Stack>
+  );
+}
