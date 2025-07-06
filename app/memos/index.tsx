93b21ad5bd83eeeeea1103beb4bee82b4fb8b973
Junichi Kako <cjaucono@gmail.com>
2025-07-06 13:42:50 +0900

    feat: memoリストの作成

--- app/memos/index.tsx
+++ app/memos/index.tsx
@@ -1,7 +1,27 @@
 import { router, useLocalSearchParams, useNavigation } from 'expo-router';
 import { useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
+import MemoListItem from '../../src/components/MemoListItem';
+
+const MEMO_DATA = [
+  {
+    id: 'ABCD',
+    name: 'useStateについて',
+    content: 'useStateについての説明',
+    label: { name: 'プログラミング', color: 'blue' },
+  },
+  {
+    id: 'EFGH',
+    name: 'アカウント',
+    content: 'メールアドレス： abcd@sample.com \n パスワード: abc123',
+  },
+  {
+    id: 'IJKL',
+    name: 'オムライス レシピ',
+    content: '卵: 2個\nご飯：200g\n玉ねぎ:1/4個\nケチャップ ',
+  },
+];
 
 export default function MemoListScreen() {
   const navigation = useNavigation();
@@ -30,14 +50,38 @@ export default function MemoListScreen() {
     router.push({ pathname: `/memos/${memoId}` });
   };
 
+  const handleMemoLongPress = (memoId: string) => {
+    console.log('メモが長押しされました', memoId);
+  };
+
+  const handleMemoDeletePress = (memoId: string) => {
+    console.log('メモが長押しされました', memoId);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         {labelId ? `ラベルID：${labelId}` : 'すべてのメモ'}
       </Text>
-      <Button title='メモ作成' onPress={handleCreatePress} />
-      <Button title='メモ１' onPress={() => handleMemoPress('ABCD')} />
-      <Button title='メモ2' onPress={() => handleMemoPress('EFGH')} />
+
+      <FlatList
+        contentContainerStyle={{ paddingBottom: 100 }}
+        data={MEMO_DATA}
+        renderItem={({ item }) => (
+          <MemoListItem
+            name={item.name}
+            content={item.content}
+            onPress={() => handleMemoPress(item.id)}
+            onDeletePress={() => handleMemoDeletePress(item.id)}
+            onLongPress={() => handleMemoLongPress(item.id)}
+            label={undefined}
+          />
+        )}
+        keyExtractor={item => item.id}
+      />
+
+      {/* <Button title='メモ作成' onPress={handleCreatePress} />
+      <Button title='メモ2' onPress={() => handleMemoPress('EFGH')} /> */}
     </View>
   );
 }
@@ -46,8 +90,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
---
+++ src/components/MemoListItem.tsx
@@ -0,0 +1,63 @@
+import { Button, ListItem } from '@rneui/themed';
+import { StyleSheet } from 'react-native';
+
+type MemoListItemProps = {
+  name: string;
+  content: string;
+  onPress: () => void;
+  onLongPress?: () => void;
+  onDeletePress?: () => void;
+  label?: { color: string; name: string };
+};
+
+export default function MemoListItem({
+  name,
+  content,
+  onPress,
+  onLongPress,
+  onDeletePress,
+  label,
+}: MemoListItemProps) {
+  return (
+    <ListItem.Swipeable
+      bottomDivider
+      onPress={onPress}
+      onLongPress={() => onLongPress?.()}
+      rightContent={reset => (
+        <Button
+          title='削除'
+          onPress={() => {
+            if (onDeletePress) {
+              onDeletePress();
+            }
+            reset();
+          }}
+          icon={{ name: 'delete', color: 'white' }}
+          buttonStyle={{ minHeight: '100%', backgroundColor: 'red' }}
+        />
+      )}
+    >
+      <ListItem.Content>
+        <ListItem.Title style={styles.title}>{name}</ListItem.Title>
+        <ListItem.Subtitle style={styles.subTitle} numberOfLines={4}>
+          {content}
+        </ListItem.Subtitle>
+      </ListItem.Content>
+      <ListItem.Chevron />
+    </ListItem.Swipeable>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#4A5054',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  subTitle: {
+    color: '#95A2AC',
+    fontSize: 15,
+    padding: 4,
+    maxHeight: 100,
+  },
+});
